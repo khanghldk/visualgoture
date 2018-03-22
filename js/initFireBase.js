29e3abc61a1f4550a627dc6a1f65a968ef2b0460
vynlt <vynlt61811@gmail.com>
2018-03-23 04:56:14 +0700

    Delete content

--- js/initFireBase.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    apiKey: "AIzaSyBk4zKJ-5eLCsVZkX1E4WgllpSO0rbG4Nw",
-    authDomain: "visualgoture.firebaseapp.com",
-    databaseURL: "https://visualgoture.firebaseio.com",
-    projectId: "visualgoture",
-    storageBucket: "visualgoture.appspot.com",
-    messagingSenderId: "764742077976"
-};
-firebase.initializeApp(config);
